2873573df11b7f701a1bb9971d125cd2098fe9ce
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-08-28 11:58:08 +0200

    [FIX] hr_holidays_public: Adapt to odoo/odoo#51542
    Since that PR, `_leave_intervals` is no longer called on days computations, so we
    intercept the batch method, but leaving the previous one for retro-compatibility.

--- hr_holidays_public/__manifest__.py
+++ hr_holidays_public/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "HR Holidays Public",
-    "version": "13.0.1.0.1",
+    "version": "13.0.1.0.2",
     "license": "AGPL-3",
     "category": "Human Resources",
     "author": "Michael Telahun Makonnen, "
