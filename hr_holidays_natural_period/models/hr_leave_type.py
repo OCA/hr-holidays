15dc583a5639a6fed6d8735161eb4aea378a5651
Víctor Martínez <victor.martinez@tecnativa.com>
2020-12-23 12:17:37 +0100

    [ADD] hr_holidays_natural_period: Add natural day in request unit leave types

---
+++ hr_holidays_natural_period/models/hr_leave_type.py
@@ -0,0 +1,10 @@
+# Copyright 2020 Tecnativa - Víctor Martínez
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+class HrLeaveType(models.Model):
+    _inherit = "hr.leave.type"
+
+    request_unit = fields.Selection(selection_add=[("natural_day", "Natural day")])
