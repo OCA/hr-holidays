81c1277fd61ea4330a4b7b9bb1352c9c6d20afa5
Janik von Rotz <login@janikvonrotz.ch>
2021-10-11 16:57:11 +0200

    hr_holidays_remining_leaves: translate and add display field

--- hr_holidays_remaining_leaves/models/hr_leave.py
+++ hr_holidays_remaining_leaves/models/hr_leave.py
@@ -1,12 +1,14 @@
 from odoo import api, fields, models, _
 import logging
 _logger = logging.getLogger(__name__)
+from odoo.tools.float_utils import float_round
 
 class HolidaysAllocation(models.Model):
     _inherit = 'hr.leave.allocation'
 
     # fields
     remaining_leaves = fields.Float(compute='_compute_remaining_leaves')
+    remaining_leaves_display = fields.Char('Remaining (Days/Hours)', compute='_compute_remaining_leaves_display')
 
     def _compute_remaining_leaves(self):
         for allocation in self:
@@ -18,3 +20,7 @@ class HolidaysAllocation(models.Model):
                 ('holiday_status_id', '=',  allocation.holiday_status_id.id)
             ])
             allocation.remaining_leaves = allocation.number_of_days - sum(leaves.mapped('number_of_days'))
+    
+    def _compute_remaining_leaves_display(self):
+        for allocation in self:
+            allocation.remaining_leaves_display = '%g %s' % (float_round(allocation.remaining_leaves, precision_digits=2), _('days'))
