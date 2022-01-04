417dc9e44a4c270464098d82907b7445056a7cdc
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2022-01-04 14:27:19 +0100

    [FIX] hr_holidays_public: Don't copy meeting_id
    If not, the date of the previous year meeting is changed for the new
    year, although a new meeting is created.
    
    It includes a test for avoiding regressions.
    
    TT33735

--- hr_holidays_public/__manifest__.py
+++ hr_holidays_public/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "HR Holidays Public",
-    "version": "14.0.2.0.2",
+    "version": "14.0.2.0.3",
     "license": "AGPL-3",
     "category": "Human Resources",
     "author": "Michael Telahun Makonnen, "
