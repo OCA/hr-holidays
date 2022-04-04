7b22ae7c9b37a1fa911ffbe8cff67ce86d9e31c5
janikvonrotz <contact@janikvonrotz.ch>
2022-04-04 16:58:20 +0200

    update readmes

--- hr_holidays_remaining_leaves/__manifest__.py
+++ hr_holidays_remaining_leaves/__manifest__.py
@@ -8,7 +8,7 @@
     'author': 'Mint System GmbH, Odoo Community Association (OCA)',
     'website': 'https://www.mint-system.ch',
     'category': 'Human Resources',
-    'version': '13.0.1.0.1',
+    'version': '14.0.1.0.0',
     'license': 'AGPL-3',
     
     'depends': ['hr_holidays'],
