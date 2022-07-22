cc812f547700f0da94d68c0097cd203892ac21ce
Víctor Martínez <victor.martinez@tecnativa.com>
2022-07-22 10:15:34 +0200

    [MIG] hr_holidays_natural_period: Migration to 14.0 TT37026
    [UPD] Update hr_holidays_natural_period.pot
    
    [UPD] README.rst

--- hr_holidays_natural_period/models/hr_leave.py
+++ hr_holidays_natural_period/models/hr_leave.py
@@ -1,20 +1,11 @@
-# Copyright 2020 Tecnativa - Víctor Martínez
+# Copyright 2020-2022 Tecnativa - Víctor Martínez
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import api, models
+from odoo import models
 
 
 class HrLeave(models.Model):
     _inherit = "hr.leave"
-
-    @api.onchange("holiday_status_id")
-    def _onchange_holiday_status_id(self):
-        res = super()._onchange_holiday_status_id()
-        if (self._origin.holiday_status_id.request_unit == "natural_day") != (
-            self.holiday_status_id.request_unit == "natural_day"
-        ):
-            self.with_context(natural_period=True)._onchange_leave_dates()
-        return res
 
     def _get_number_of_days(self, date_from, date_to, employee_id):
         instance = self.with_context(
