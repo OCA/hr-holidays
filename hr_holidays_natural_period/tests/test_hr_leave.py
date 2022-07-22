6aec21c0f8760a90c83591db32b67552f62ef15f
Víctor Martínez <victor.martinez@tecnativa.com>
2022-07-22 10:15:34 +0200

    [MIG] hr_holidays_natural_period: Migration to 14.0 TT37026

--- hr_holidays_natural_period/tests/test_hr_leave.py
+++ hr_holidays_natural_period/tests/test_hr_leave.py
@@ -1,4 +1,4 @@
-# Copyright 2020 Tecnativa - Víctor Martínez
+# Copyright 2020-2022 Tecnativa - Víctor Martínez
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo.tests import Form, common
@@ -53,7 +53,7 @@ class TestHrLeave(common.SavepointCase):
         leave_form.holiday_status_id = self.leave_type
         leave_form.request_date_from = "2021-01-02"
         leave_form.request_date_to = "2021-01-05"
-        self.assertEquals(leave_form.number_of_days, 4.0)
+        self.assertEqual(leave_form.number_of_days, 4.0)
 
     def test_hr_leave_day(self):
         leave_form = Form(
@@ -64,4 +64,4 @@ class TestHrLeave(common.SavepointCase):
         leave_form.holiday_status_id = self.env.ref("hr_holidays.holiday_status_cl")
         leave_form.request_date_from = "2021-01-02"  # Saturday
         leave_form.request_date_to = "2021-01-04"  # Monday
-        self.assertEquals(leave_form.number_of_days, 1)
+        self.assertEqual(leave_form.number_of_days, 1)
