b157e4305335ceff0bc8bb2f4c4abc8b775eb682
rafaferri <rafa.ferri@studio73.es>
2023-05-05 13:31:09 +0200

    [MIG] hr_leave_custom_hour_interval: migration to 16.0

--- hr_leave_custom_hour_interval/__manifest__.py
+++ hr_leave_custom_hour_interval/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Custom Hour Interval for Holidays",
     "summary": """
         Edit start and end of leaves using time intervals""",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "Creu Blanca,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/hr-holidays",
