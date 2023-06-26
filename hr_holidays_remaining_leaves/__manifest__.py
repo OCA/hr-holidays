33ae6f1d22cb4de18cab6b891ed9ff5bd8e84666
Janik von Rotz <login@janikvonrotz.ch>
2023-06-26 10:10:20 +0200

    [hr_employee_attendance_report] migrate [hr_holidays_remaining_leaves] migrate

---
+++ hr_holidays_remaining_leaves/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "HR Holidays Remaining Leaves",
+    "summary": """
+        Show remaining leaves per employee in allocation overview.
+    """,
+    "author": "Mint System GmbH, Odoo Community Association (OCA)",
+    "website": "https://www.mint-system.ch",
+    "category": "Human Resources",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["hr_holidays"],
+    "data": ["views/hr_leave_allocation.xml"],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "images": ["images/screen.png"],
+}
