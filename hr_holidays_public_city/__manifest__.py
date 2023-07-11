edc0e2959ca031e731a0d028fdb845ea36953a42
Víctor Martínez <victor.martinez@tecnativa.com>
2023-07-11 18:00:36 +0200

    [ADD] hr_holidays_public_city: New module
    TT43035
    
    [UPD] Update hr_holidays_public_city.pot
    
    [UPD] README.rst
    
    hr_holidays_public_city 15.0.1.0.1

---
+++ hr_holidays_public_city/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2023 Tecnativa - Víctor Martínez
+# Copyright 2023 Tecnativa - Pedro M. Baeza
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "HR Holidays Public City",
+    "version": "15.0.1.0.1",
+    "category": "Tools",
+    "website": "https://github.com/OCA/hr-holidays",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": ["hr_holidays_public", "base_address_city"],
+    "data": [
+        "views/hr_holidays_public_view.xml",
+    ],
+    "installable": True,
+    "maintainers": ["victoralmau"],
+}
