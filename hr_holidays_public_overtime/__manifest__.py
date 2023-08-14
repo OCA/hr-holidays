30aaca7c014b765e8ee5065e1c5cbe64a18d3137
Holger Brunn <mail@hunki-enterprises.com>
2023-08-14 16:54:48 +0200

    [ADD] hr_holidays_public_overtime

---
+++ hr_holidays_public_overtime/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2023 Hunki Enterprises BV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl-3.0)
+
+{
+    "name": "HR Holidays Public (overtime)",
+    "summary": "Support public holidays in overtime calculations",
+    "version": "15.0.1.0.1",
+    "development_status": "Beta",
+    "category": "Human Resources",
+    "website": "https://github.com/OCA/hr-holidays",
+    "author": "Hunki Enterprises BV, Odoo Community Association (OCA), verdigado eG",
+    "maintainers": ["hbrunn"],
+    "license": "AGPL-3",
+    "depends": [
+        "hr_holidays_public",
+        "hr_attendance",
+    ],
+    "auto_install": True,
+}
