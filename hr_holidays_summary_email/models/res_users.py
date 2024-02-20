75692d11ad40043e6ae6e410020c691a83fb7bf0
JordiMForgeFlow <jordi.masvidal@forgeflow.com>
2024-02-20 10:22:31 +0100

    [FIX] hr_holidays_summary_email: allow users to self edit setting

--- hr_holidays_summary_email/models/res_users.py
+++ hr_holidays_summary_email/models/res_users.py
@@ -9,3 +9,17 @@ class ResUser(models.Model):
     hr_leave_summary_type = fields.Selection(
         related="employee_id.hr_leave_summary_type", readonly=False, related_sudo=False
     )
+
+    def __init__(self, pool, cr):
+        """Override of __init__ to add access rights.
+        Access rights are disabled by default, but allowed
+        on some specific fields defined in self.SELF_{READ/WRITE}ABLE_FIELDS.
+        """
+        super(ResUser, self).__init__(pool, cr)
+        # duplicate list to avoid modifying the original reference
+        type(self).SELF_READABLE_FIELDS = type(self).SELF_READABLE_FIELDS + [
+            "hr_leave_summary_type"
+        ]
+        type(self).SELF_WRITEABLE_FIELDS = type(self).SELF_WRITEABLE_FIELDS + [
+            "hr_leave_summary_type"
+        ]
