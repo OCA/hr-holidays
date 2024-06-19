9a0b2c5d09eb5c709a6b5de21020fabddee523bb
sonhd91 <sonhd@trobz.com>
2024-06-19 17:28:33 +0700

    [IMP] resource_calendar_get_days: pre-commit auto fixes

--- resource_calendar_get_days/models/resource_calendar.py
+++ resource_calendar_get_days/models/resource_calendar.py
@@ -1,5 +1,6 @@
-from odoo import models
 from collections import defaultdict
+
+from odoo import models
 from odoo.tools import float_utils
 
 HOURS_PER_DAY = 8
@@ -16,14 +17,19 @@ class ResourceCalendar(models.Model):
         day_hours = defaultdict(float)
         for start, stop, meta in intervals:
             day_hours[start.date()] += (stop - start).total_seconds() / 3600
-        
+
         # compute number of days as quarters
         hours_per_day = self.company_id.resource_calendar_id.hours_per_day
         days = sum(
-            float_utils.round(ROUNDING_FACTOR * day_hours[day] / hours_per_day or day_total[day]) / ROUNDING_FACTOR if day_total[day] else 0
+            float_utils.round(
+                ROUNDING_FACTOR * day_hours[day] / hours_per_day or day_total[day]
+            )
+            / ROUNDING_FACTOR
+            if day_total[day]
+            else 0
             for day in day_hours
         )
         return {
-            'days': days,
-            'hours': sum(day_hours.values()),
+            "days": days,
+            "hours": sum(day_hours.values()),
         }
