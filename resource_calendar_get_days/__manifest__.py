26f2526d47d6d35221410978b7ade4bbb6d375ed
sonhd91 <sonhd@trobz.com>
2024-06-19 17:28:33 +0700

    [IMP] resource_calendar_get_days: pre-commit auto fixes

--- resource_calendar_get_days/__manifest__.py
+++ resource_calendar_get_days/__manifest__.py
@@ -4,11 +4,11 @@
         Calculate number of leave days relative to company working hours.
     """,
     "author": "Mint System GmbH, Odoo Community Association (OCA)",
-    "website": "https://www.mint-system.ch",
+    "website": "https://github.com/OCA/hr-holidays",
     "category": "Human Resources",
     "version": "14.0.2.0.0",
     "license": "AGPL-3",
-    "demo":  ["demo/resource_data.xml"],
+    "demo": ["demo/resource_data.xml"],
     "depends": ["hr_holidays"],
     "installable": True,
     "application": False,
