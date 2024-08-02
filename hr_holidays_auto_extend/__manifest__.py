7cb6d6621abba6fb25315637c190d5b09d1c1f4a
Luis Rodriguez <luis.rodriguez@dixmit.com>
2024-08-02 09:15:36 +0200

    [MIG] hr_holidays_auto_extend: Migration to 16.0

--- hr_holidays_auto_extend/__manifest__.py
+++ hr_holidays_auto_extend/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Hr Holidays Auto Extend",
     "summary": """
         Allow to extend some kind of holidays""",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "CreuBlanca,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/hr-holidays",
