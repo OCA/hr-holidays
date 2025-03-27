877b60d1ce75969d0b95fb2bce1a2a81b0b6f6e8
Víctor Martínez <victor.martinez@tecnativa.com>
2025-03-27 09:19:16 +0100

    [MIG] hr_holidays_natural_period: Migration to 18.0
    TT55532

--- hr_holidays_natural_period/models/hr_leave.py
+++ hr_holidays_natural_period/models/hr_leave.py
@@ -1,4 +1,4 @@
-# Copyright 2020-2024 Tecnativa - Víctor Martínez
+# Copyright 2020-2025 Tecnativa - Víctor Martínez
 # Copyright 2024 Tecnativa - Carlos Lopez
 # Copyright 2025 Grupo Isonor - Alexandre D. Díaz
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
@@ -9,20 +9,26 @@ from odoo import models
 class HrLeave(models.Model):
     _inherit = "hr.leave"
 
-    def _get_duration(self, check_leave_type=True, resource_calendar=None):
+    def _get_durations(self, check_leave_type=True, resource_calendar=None):
         # We need to set request_unit as 'day'
         # to avoid the calculations being done as hours.
         mod_holidays_status_ids = self.env.context.get("mod_holidays_status_ids", [])
-        is_mod_leave_type = self.holiday_status_id.id in mod_holidays_status_ids
-        is_request_unit_natural_day = is_mod_leave_type or (
-            self.holiday_status_id.request_unit == "natural_day"
+        natural_day_instances = self.filtered(
+            lambda x: x.holiday_status_id.id in mod_holidays_status_ids
+            or x.holiday_status_id.request_unit == "natural_day"
         )
-        instance = self.with_context(natural_period=is_request_unit_natural_day)
-        if is_request_unit_natural_day:
-            self.holiday_status_id.sudo().request_unit = "day"
-        res = super(HrLeave, instance)._get_duration(
+        natural_day_instances.holiday_status_id.sudo().request_unit = "day"
+        res = super(HrLeave, (self - natural_day_instances))._get_durations(
             check_leave_type=check_leave_type, resource_calendar=resource_calendar
         )
-        if is_request_unit_natural_day:
-            self.holiday_status_id.sudo().request_unit = "natural_day"
+        if not natural_day_instances:
+            return res
+        _res = super(
+            HrLeave, natural_day_instances.with_context(natural_period=True)
+        )._get_durations(
+            check_leave_type=check_leave_type, resource_calendar=resource_calendar
+        )
+        for item in natural_day_instances:
+            res[item.id] = _res[item.id]
+        natural_day_instances.holiday_status_id.sudo().request_unit = "natural_day"
         return res
--- hr_holidays_natural_period/tests/test_hr_leave.py
+++ hr_holidays_natural_period/tests/test_hr_leave.py
@@ -2,14 +2,14 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from freezegun import freeze_time
 
-from odoo import fields
+from odoo import Command, fields
 from odoo.tests import Form, new_test_user
 from odoo.tests.common import users
+from odoo.tools import mute_logger
 
 from odoo.addons.base.tests.common import BaseCommon
 
 
-@freeze_time("2023-01-01", tick=True)
 class TestHrLeave(BaseCommon):
     @classmethod
     def setUpClass(cls):
@@ -53,10 +53,9 @@ class TestHrLeave(BaseCommon):
         leave_allocation_form = Form(
             self.env["hr.leave.allocation"].with_context(
                 default_date_from="2023-01-01",
-                default_date_to="%s-12-31" % (fields.Date.today().year),
+                default_date_to=f"{fields.Date.today().year}-12-31",
             )
         )
-
         leave_allocation_form.holiday_status_id = leave_type
         leave_allocation_form.number_of_days_display = days
         return leave_allocation_form.save()
@@ -69,6 +68,8 @@ class TestHrLeave(BaseCommon):
         return leave_form.save()
 
     @users("test-user")
+    @freeze_time("2023-01-01", tick=True)
+    @mute_logger("odoo.models.unlink")
     def test_hr_leave_natural_day_01(self):
         leave_allocation = self._create_leave_allocation(self.leave_type, 5)
         leave_allocation.sudo().action_validate()
@@ -85,9 +86,10 @@ class TestHrLeave(BaseCommon):
         self.assertEqual(res_leave_type["request_unit"], "natural_day")
         leave = self._create_hr_leave(self.leave_type, "2023-01-02", "2023-01-05")
         self.assertEqual(leave.number_of_days, 4.0)
-        self.assertEqual(leave.number_of_days_display, 4.0)
 
     @users("test-user")
+    @freeze_time("2023-01-01", tick=True)
+    @mute_logger("odoo.models.unlink")
     def test_hr_leave_natural_day_02(self):
         attendances = [(0, 16, 21), (1, 9, 14), (2, 9, 14), (3, 9, 14), (4, 9, 14)]
         r_sudo = self.env["resource.calendar"].sudo()
@@ -96,9 +98,7 @@ class TestHrLeave(BaseCommon):
                 "name": "Test calendar",
                 "tz": "Europe/Brussels",
                 "attendance_ids": [
-                    (
-                        0,
-                        0,
+                    Command.create(
                         {
                             "name": index,
                             "dayofweek": str(att[0]),
@@ -113,9 +113,10 @@ class TestHrLeave(BaseCommon):
         self.employee.resource_calendar_id = calendar
         leave = self._create_hr_leave(self.leave_type, "2022-12-31", "2023-01-08")
         self.assertEqual(leave.number_of_days, 9.0)
-        self.assertEqual(leave.number_of_days_display, 9.0)
 
     @users("test-user")
+    @freeze_time("2023-01-01", tick=True)
+    @mute_logger("odoo.models.unlink")
     def test_hr_leave_day(self):
         leave_allocation = self._create_leave_allocation(self.leave_type_day, 5)
         leave_allocation.sudo().action_validate()
@@ -132,4 +133,3 @@ class TestHrLeave(BaseCommon):
         self.assertEqual(res_leave_type["request_unit"], "day")
         leave = self._create_hr_leave(self.leave_type_day, "2023-01-08", "2023-01-15")
         self.assertEqual(leave.number_of_days, 5)
-        self.assertEqual(leave.number_of_days_display, 5)
