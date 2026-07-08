4f7407c926940dc0366475b10dce1ad9f1966ba4
Borruso <gborruso@dinamicheaziendali.it>
2026-07-08 15:38:50 +0200

    [IMP] hr_holidays_leave_auto_approve: pre-commit auto fixes

--- hr_holidays_leave_auto_approve/tests/test_hr_holidays_leave_auto_approve.py
+++ hr_holidays_leave_auto_approve/tests/test_hr_holidays_leave_auto_approve.py
@@ -63,7 +63,6 @@ class TestHolidaysAutoValidate(TransactionCase):
         self.assertEqual(self.leave_allocation2.state, "validate")
 
     def test_leave_requests_state(self):
-
         today = datetime.today()
 
         # Create leave requests for Leave Type1 and 2
@@ -96,7 +95,6 @@ class TestHolidaysAutoValidate(TransactionCase):
         self.assertEqual(leave2.state, "confirm")
 
     def test_leave_requests_state_employee_user(self):
-
         today = datetime.today()
 
         # Create leave requests for Leave Type1 and 2
@@ -129,7 +127,6 @@ class TestHolidaysAutoValidate(TransactionCase):
         self.assertEqual(leave2.state, "confirm")
 
     def test_leave_request_employee_validate_all(self):
-
         self.test_user_id.groups_id = [(6, 0, [self.env.ref("base.group_user").id])]
 
         today = datetime.today()
